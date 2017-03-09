03492bab0fbabb66ccf43deae60a8de52fe307d3
Robin <Robin@HALOMOBILE>
2017-03-09 19:10:20 +0100

    Erstellung Teil2

---
+++ SophosAPI/SophosAPI/Classes/SGCredentials.cs
@@ -0,0 +1,8 @@
+﻿namespace SophosAPI
+{
+    public class SGCredentials
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ SophosAPI/SophosAPI/Functions/NewSGCredentialsCmdlet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+namespace SophosAPI.Functions
+{
+    [Cmdlet(VerbsCommon.New, "SGCredentials")]
+    [OutputType(typeof(SGCredentials))]
+    public class NewSGCredentialsCmdlet : Cmdlet
+    {
+        
+    }
+}
